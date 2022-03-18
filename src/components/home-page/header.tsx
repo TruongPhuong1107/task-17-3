bf4915afb60b130d238e1acf25729d2501251c2d
hoangtrungnamus <nampanama0101@gmail.com>
2022-03-18 16:38:15 +0700

    resolve conflict

--- src/components/home-page/header.tsx
+++ src/components/home-page/header.tsx
@@ -3,41 +3,26 @@ import Link from "next/link";
 
 const HeaderHomePage = () => {
   return (
-<<<<<<< HEAD
-    <div className='xl:children:mb-7 font-fred children:my-4'>
-      <h1 className='xl:text-8xl font-bold text-maincolor text-7xl'>
+    <div className="xl:children:mb-7 font-fred children:my-4">
+      <h1 className="xl:text-8xl font-bold text-maincolor ">
         Hosting solution with benefits
       </h1>
-      <p className='xl:text-4xl text-3xl'>
+      <p className="xl:text-4xl text-3xl">
         Turn your ideas reality with Static. <br />
         With a lot of powerful features, we guarantee simplicity and clarity.
       </p>
-      <p className='xl:text-4xl text-3xl'>
-        Image from <span className=' border-b border-b-slate-900'>Freepik</span>
-      </p>
-      <button className='xl:w-60 xl:py-4 xl:text-2xl text-white bg-maincolor rounded-full tracking-wider text-2xl w-56 py-2'>
-        Read More
-      </button>
-=======
-    <div className="children:my-5 font-sans">
-      <h1 className="text-8xl font-bold text-maincolor ">
-        Hosting solution with benefits
-      </h1>
-      <p className="text-4xl">
-        Turn your ideas reality with Static. <br />
-        With a lot of powerful features, we guarantee simplicity and clarity.
-      </p>
-      <p className="text-4xl">
+      <p className="xl:text-4xl text-3xl">
         Image from <span className=" border-b border-b-slate-900">Freepik</span>
       </p>
-      <Link href="/details-page">
-        <a>
-          <button className="w-60 py-4 h-fit text-2xl text-white bg-maincolor rounded-full tracking-wider">
-            Read More
-          </button>
-        </a>
-      </Link>
->>>>>>> nam
+      <button className="xl:w-60 xl:py-4 xl:text-2xl text-white bg-maincolor rounded-full tracking-wider ">
+        <Link href="/details-page">
+          <a>
+            <button className="h-fit text-white bg-maincolor rounded-full tracking-wider text-2xl w-56 py-2">
+              Read More
+            </button>
+          </a>
+        </Link>
+      </button>
     </div>
   );
 };
